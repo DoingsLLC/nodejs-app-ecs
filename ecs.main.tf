96f9c832d951efb8cf4eee883f8b83357812ccd5
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-11-12 08:27:29 -0600

    Update ecs.main.tf

--- ecs.main.tf
+++ ecs.main.tf
@@ -41,7 +41,7 @@ resource "aws_ecs_service" "doings_ecs_service" {
   launch_type     = "FARGATE"
   network_configuration {
     subnets          = ["subnet-0659d87ad02d0a823"]  
-    security_groups  = ["sg-0dab44e5556cb4879"]      
+    security_groups  = ["sg-06a74748260eaf868"]      
     assign_public_ip = true
   }
 }
