bdaeeb68bb24bc51fd546aee43c276d2dea01a9e
Peter Obijiofor <108196402+DoingsLLC@users.noreply.github.com>
2023-11-14 12:11:58 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,20 @@
 provider "aws" {
-  region = "us-east-1"  # Adjust the region as needed
+  region = "us-east-1"
 }
 
-resource "aws_ecs_cluster" "my_cluster" {
+resource "aws_ecs_cluster" "doings_cluster" {
   name = "doings-ecs-cluster"
 }
 
-resource "aws_ecs_task_definition" "my_task_definition" {
-  family                  = "my-task-family"
+resource "aws_ecs_task_definition" "doings_task" {
+  family                   = "doings-task" 
   requires_compatibilities = ["FARGATE"]
-  network_mode            = "awsvpc"
-  cpu                     = 256
-  memory                  = 512
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
-
-  container_definitions = <<EOF
+  container_definitions = <<DEFINITION
 [
   {
     "name": "doings-container",
@@ -23,22 +22,23 @@ resource "aws_ecs_task_definition" "my_task_definition" {
     "portMappings": [
       {
         "containerPort": 80,
-        "hostPort": 80
+        "hostPort": 8080 
       }
     ]
   }
 ]
-EOF
+DEFINITION
+
 }
 
-resource "aws_ecs_service" "ecs_service" {
-  name            = "dongs-ecs-service"
-  cluster         = aws_ecs_cluster.my_cluster.id
-  task_definition = aws_ecs_task_definition.my_task_definition.arn
+resource "aws_ecs_service" "doings_service" {
+  name            = "doings-service"
+  cluster         = aws_ecs_cluster.doings_cluster.id
+  task_definition = aws_ecs_task_definition.doings_task.arn
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets          = ["subnet-0aa0739f3f084ad3e"]  # Adjust the subnet as needed
+    subnets          = ["subnet-0aa0739f3f084ad3e"]
     assign_public_ip = true
   }
 }
@@ -46,18 +46,39 @@ resource "aws_ecs_service" "ecs_service" {
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "doings-ecs-task-execution-role"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ecs-tasks.amazonaws.com"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com" 
+        }
       },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
+    ]
+  })
+  
+  inline_policy {
+    name = "doings-ecs-task-permissions"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = [
+            "ecr:GetAuthorizationToken",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]  
+    })
+  }
 }
